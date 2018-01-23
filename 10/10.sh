18d48d104dc2be2d71e6f35cda3b7025f911f95f
Alfredo Casanova <atcasanova@gmail.com>
2018-01-23 15:56:24 -0200

    Update 10.sh

--- 10/10.sh
+++ 10/10.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-strings chest  | grep -P "cm18((?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-])"
+#strings chest  | grep -P "cm18((?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d-])"
+strings chest | grep cm18 | grep -vE -- "-[a-z]{4}$"
